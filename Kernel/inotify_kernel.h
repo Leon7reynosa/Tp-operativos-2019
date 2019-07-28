40603441565078be3e97f72836b8796b083c2482
leon7reynosa <leonreynosa98@gmail.com>
2019-07-28 17:45:19 -0300

    agrgando semaforos de las memorias en el kernel

---
+++ Kernel/inotify_kernel.h
@@ -0,0 +1,35 @@
+/*
+ * inotify_kernel.h
+ *
+ *  Created on: 28 jul. 2019
+ *      Author: utnso
+ */
+
+#ifndef INOTIFY_KERNEL_H_
+#define INOTIFY_KERNEL_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<stdbool.h>
+#include<errno.h>
+#include<sys/inotify.h>
+#include<pthread.h>
+
+#include"Config/configuracion.h"
+
+#include<commons/config.h>
+
+struct inotifyEstructura{
+
+	int fd_inotify;
+	int fd_watch;
+	char* path_config;
+
+};
+
+typedef struct inotifyEstructura* inotify_config;
+
+void* realizar_inotify(inotify_config argumento);
+
+#endif /* INOTIFY_KERNEL_H_ */
